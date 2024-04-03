e20a4cff2b3fa4c5c87f3d78a73962ca2d85904a
Kim Daniel <kimdaniiell@gmail.com>
2024-04-03 03:07:23 +0300

    mvn things

--- src/main/java/ru/itmo/hsgateway/configuration/GatewayConfiguration.java
+++ src/main/java/ru/itmo/hsgateway/configuration/GatewayConfiguration.java
@@ -28,6 +28,48 @@ public class GatewayConfiguration {
                                         .filters(factory.apply(new AuthenticationFilterFactory.Config()))
                                         .prefixPath("/api"))
                                 .uri("lb://auth-service"))
+                .route("playgrounds-service-playgrounds",
+                        r -> r
+                                .path("/playgrounds", "/playgrounds/**")
+                                .filters(f -> f
+                                        .filters(factory.apply(new AuthenticationFilterFactory.Config()))
+                                        .prefixPath("/api"))
+                                .uri("lb://playgrounds-service"))
+                .route("playgrounds-service-sports",
+                        r -> r
+                                .path("/sports", "/sports/**")
+                                .filters(f -> f
+                                        .filters(factory.apply(new AuthenticationFilterFactory.Config()))
+                                        .prefixPath("/api"))
+                                .uri("lb://playgrounds-service"))
+                .route("actors-service-managers",
+                        r -> r
+                                .path("/managers", "/managers/**")
+                                .filters(f -> f
+                                        .filters(factory.apply(new AuthenticationFilterFactory.Config()))
+                                        .prefixPath("/api"))
+                                .uri("lb://actors-service"))
+                .route("actors-service-players",
+                        r -> r
+                                .path("/players", "/players/**")
+                                .filters(f -> f
+                                        .filters(factory.apply(new AuthenticationFilterFactory.Config()))
+                                        .prefixPath("/api"))
+                                .uri("lb://actors-service"))
+                .route("actors-service-teams",
+                        r -> r
+                                .path("/teams", "/teams/**")
+                                .filters(f -> f
+                                        .filters(factory.apply(new AuthenticationFilterFactory.Config()))
+                                        .prefixPath("/api"))
+                                .uri("lb://actors-service"))
+                .route("booking-service-bookings",
+                        r -> r
+                                .path("/bookings", "/bookings/**")
+                                .filters(f -> f
+                                        .filters(factory.apply(new AuthenticationFilterFactory.Config()))
+                                        .prefixPath("/api"))
+                                .uri("lb://booking-service"))
                 .build();
     }
 }
